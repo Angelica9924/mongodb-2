bedd2b0e7de2a49a16048ffa7d255ad61b8c7ecd
Angelica9924 <angelicamartinez.c@outlook.com>
2024-03-17 05:01:44 -0500

    agg taller 2

---
+++ db2.js
@@ -0,0 +1,371 @@
+db.createCollection('usuarios2');
+
+db.usuarios2.insertMany([
+    {
+        name: 'Valeria',
+        lastname: 'Perez',
+        email: 'vale@outlook.com',
+        city: 'Medellin',
+        country: 'Colombia',
+        salary: 2500,
+        age: 27,
+        height: 183,
+        weight: 187 
+    },
+    {
+        name: 'Juan',
+        lastname: 'Velez',
+        email: 'juanve@outlook.com',
+        city: 'New York',
+        country: 'USA',
+        salary: 5000,
+        age: 35,
+        height: 170,
+        weight: 165.35 
+    },
+    {
+        name: 'Elcy',
+        lastname: 'Carmona',
+        email: 'carmonael@outlook.com',
+        city: 'London',
+        country: 'UK',
+        salary: 6000,
+        age: 15,
+        height: 181,
+        weight: 176
+    },
+    {
+        name: 'Sofia',
+        lastname: 'Quintero',
+        email: 'sofiequin@outlook.com',
+        city: 'Paris',
+        country: 'France',
+        salary: 4600,
+        age: 28,
+        height: 170,
+        weight: 135
+    },
+    {
+        name: 'Ukele',
+        lastname: 'Kali',
+        email: 'ukele@outlook.com',
+        city: 'Bangalore',
+        country: 'India',
+        age: 25
+    },
+    {
+        name: 'Rafa',
+        lastname: 'Piedrahita',
+        email: 'rafael@outlook.com',
+        city: 'Mexico City',
+        country: 'Mexico',
+        salary: 2800,
+        age: 34,
+        height: 150,
+        weight: 165
+    },
+    {
+        name: 'Eugenia',
+        lastname: 'Molina',
+        email: 'eua.molina@outlook.com',
+        city: 'Berlin',
+        country: 'Germany',
+        salary: 3900,
+        age: 31,
+        height: 165,
+        weight: 145
+    },
+    {
+        name: 'Albeiro',
+        lastname: 'Carmona',
+        email: 'albecarm@outlook.com',
+        city: 'Rome',
+        country: 'Italy',
+        salary: 2950,
+        age: 25,
+        height: 174,
+        weight: 155
+    },
+    {
+        name: 'Helena',
+        lastname: 'Marin',
+        email: 'helenama@outlook.com',
+        city: 'Warsaw',
+        country: 'Poland',
+        salary: 3150,
+        age: 27,
+        height: 174,
+        weight: 140
+    },
+    {
+        name: 'Lua',
+        lastname: 'Ner',
+        email: 'lua@outlook.com',
+        city: 'Seoul',
+        country: 'South Korea',
+        age: 25
+    },
+    {
+        name: 'Valentina',
+        lastname: 'Restrepo',
+        email: 'valen@outlook.com',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+        salary: 2900,
+        age: 31,
+        height: 172,
+        weight: 175
+    },
+    {
+        name: 'Juanjo',
+        lastname: 'Rincon',
+        email: 'juanjorin@outlook.com',
+        city: 'Buenos Aires',
+        country: 'Argentina',
+        salary: 3100,
+        age: 30,
+        height: 163,
+        weight: 154
+    },
+    {
+        name: 'Diana',
+        lastname: 'Toro',
+        email: 'dianatoro@outlook.com',
+        city: 'Barcelona',
+        country: 'Spain',
+        salary: 4300,
+        age: 22,
+        height: 165,
+        weight: 132.28 
+    },
+    {
+        name: 'Jhonatan',
+        lastname: 'Martinez',
+        email: 'martinez@outlook.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 5500,
+        age: 40,
+        height: 160,
+        weight: 121
+    },
+    {
+        name: 'Jose',
+        lastname: 'Ortega',
+        email: 'joseort@outlook.com',
+        city: 'Los Angeles',
+        country: 'USA',
+        salary: 7000,
+        age: 17,
+        height: 160,
+        weight: 154.32 
+    },
+    {
+        name: 'Wilmar',
+        lastname: 'Lee',
+        email: 'wilmar@outlook.com',
+        city: 'Seoul',
+        country: 'South Korea',
+        salary: 2800,
+        age: 38,
+        height: 153,
+        weight: 127.87 
+    },
+    {
+        name: 'Juanda',
+        lastname: 'cehn',
+        email: 'juan@outlook.com',
+        city: 'Chi City',
+        country: 'Vietnam',
+        salary: 4000,
+        age: 41,
+        height: 168,
+        weight: 143.3 
+    },
+    {
+        name: 'Isabella',
+        lastname: 'Marin',
+        email: 'isabella@outlook.com',
+        city: 'Mexico City',
+        country: 'Mexico',
+        salary: 3400,
+        age: 29,
+        height: 160,
+        weight: 134.89 
+    },
+    {
+        name: 'Jackie',
+        lastname: 'Chan',
+        email: 'jackie.chan@outlook.com',
+        city: 'Shanghai',
+        country: 'China',
+        salary: 6000,
+        age: 16,
+        height: 175,
+        weight: 158.73 
+    },
+    {
+        name: 'Alejandra',
+        lastname: 'Gomez',
+        email: 'gomez@outlook.com',
+        city: 'Bogotá',
+        country: 'Colombia',
+        salary: 2800,
+        age: 30,
+        height: 162,
+        weight: 132.28 
+    },
+    {
+        name: 'Heriberto',
+        lastname: 'Perez',
+        email: 'heriperez@outlook.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 3200,
+        age: 28,
+        height: 173,
+        weight: 160
+    },
+    {
+        name: 'Nestor',
+        lastname: 'González',
+        email: 'ejemplo@riwi.es',
+        city: 'Medellin',
+        country: 'Colombia',
+        salary: 2500,
+        age: 18,
+        height: 183,
+        weight: 187 
+    },
+    {
+        name: 'Pedro',
+        lastname: 'González',
+        email: 'ejemplo@riwi.es',
+        city: 'Medellin',
+        country: 'Colombia',
+        salary: 2500,
+        age: 18,
+        height: 183,
+        weight: 187 
+    },
+    {
+        name: 'Maria',
+        lastname: 'González',
+        email: 'ejemplo@riwi.es',
+        city: 'Medellin',
+        country: 'Colombia',
+        salary: 2500,
+        age: 18,
+        height: 183,
+        weight: 187 
+    },
+    {
+        name: 'Nestor',
+        lastname: 'González',
+        email: 'otro@riwi.net',
+        city: 'Medellin',
+        country: 'Colombia',
+        salary: 2500,
+        age: 18,
+        height: 183,
+        weight: 187 
+    },
+    {
+        name: 'Juan',
+        lastname: 'Perez',
+        email: 'otro@riwi.net',
+        city: 'Medellin',
+        country: 'Colombia',
+        salary: 2500,
+        age: 18,
+        height: 183,
+        weight: 187 
+    }
+]);
+
+
+//ELIMINAR
+//1.Eliminar todos los usuarios que tienen un salario menor que 2000 dólares.
+db.usuarios2.deleteMany({salary:{$lt:2000}});
+//2.Eliminar usuarios que tienen una edad menor que 25 años.
+db.usuarios2.deleteMany({age:{$lt:25}});
+//3.Borrar todos los usuarios que viven en "París".
+db.usuarios2.deleteMany({city:{$eq:"Paris"}});
+//4.Eliminar usuarios que tienen un peso superior a 180 libras.
+db.usuarios2.deleteMany({weight:{$gt:180}});
+//5.Eliminar usuarios que tienen una altura inferior a 160 centímetros.
+db.usuarios2.deleteMany({height:{$lt:160}});
+//6.Eliminar todos los usuarios que tienen el nombre "John" y el apellido "Doe".
+db.usuarios2.deleteMany({$and:[{name:{$eq:"John"}},{lastname:{$eq:"Doe"}}]});
+//7.Borrar usuarios que tienen una dirección de correo electrónico específica, por ejemplo, "borrar@riwi.com".
+db.usuarios2.deleteMany({email:{$eq:"borrar@riwi.com"}});
+//8.Eliminar usuarios que no tienen una dirección de correo electrónico registrada.
+db.usuarios2.deleteMany({email: {$exists:false}});
+//9.Eliminar usuarios que viven en "Tokyo".
+db.usuarios2.deleteMany({city:{$eq:"Tokyo"}});
+//10.Borrar todos los usuarios que son menores de 18 años.
+db.usuarios2.deleteMany({age:{$lt:18}});
+//11.Eliminar usuarios que tienen un salario igual a 0.
+db.usuarios2.deleteMany({salary:{$eq:0}});
+//12.Borrar usuarios que viven en "New York" y tienen un salario superior a 5000 dólares.
+db.usuarios2.deleteMany({$and:[{city:{$eq:"New York"}},{salary:{$gt:5000}}]});
+//13.Eliminar usuarios que tienen una dirección de correo electrónico que contiene la palabra "spam".
+db.usuarios2.deleteMany({email:{$regex:/spam/i}});
+//14.Borrar usuarios que viven en "Bello" y tienen más de 50 años.
+db.usuarios2.deleteMany({$and:[{city:{$eq:"Bello"}},{age:{$gt:50}}]});
+//15.Eliminar todos los usuarios que tienen el apellido "González".
+db.usuarios2.deleteMany({lastname:{$eq:"González"}});
+//16.Borrar usuarios que tienen una edad superior a 70 años.
+db.usuarios2.deleteMany({age:{$gt:70}});
+//17.Eliminar usuarios que tienen un país de residencia igual a "Canadá" y un salario inferior a 4000 dólares.
+db.usuarios2.deleteMany({$and:[{country:{$eq:"Canadá"}},{salary:{$lt:4000}}]});
+//18.Borrar usuarios que tienen un salario entre 1000 y 2000 dólares.
+db.usuarios2.deleteMany({$and:[{salary:{$gte:1000}},{salary:{$lte:2000}}]});
+//19.Eliminar usuarios que tienen una edad igual a 30 años.
+db.usuarios2.deleteMany({age:{$eq:30}});
+//20.Borrar usuarios que tienen una altura superior a 190 centímetros.
+db.usuarios2.deleteMany({height:{$gt:190}});
+
+//ACTUALIZAR
+//1.Incrementar el salario de todos los usuarios en un 10%.
+db.usuarios2.updateMany({},{$mul:{salary:1.1}});
+db.usuarios2.find();
+db.usuarios2.drop();
+//2.Cambiar la ciudad de residencia de los usuarios que actualmente viven en "New York" a "Los Ángeles".
+db.usuarios2.updateMany({city:"New York"},{$set:{city:"Los Ángeles"}});
+//3.Agregar el correo electrónico "nuevo@riwi.com" al usuario con nombre "Juan" y apellido "Perez".
+db.usuarios2.updateMany({$and:[{name:"Juan"},{lastname:"Perez"}]},{$set:{email:"nuevo@riwi.com"}});
+//4.Actualizar la edad del usuario con correo electrónico "ejemplo@riwi.es" a 35 años.
+db.usuarios2.updateMany({email:"ejemplo@riwi.es"},{$set:{age:35}});
+//5.Cambiar el país de residencia de los usuarios que son de "Mexico" a "Canada".
+db.usuarios2.updateMany({country:"Mexico"},{$set:{country:"Canadá"}});
+//6.Aumentar la altura de todos los usuarios en 5 centímetros.
+db.usuarios2.updateMany({}, {$inc: {height: 5}});
+//7.Cambiar el apellido del usuario con correo electrónico "otro@riwi.net" a "González".
+db.usuarios2.updateMany({email:"otro@riwi.net"},{$set:{lastname:"González"}});
+//8.Actualizar el peso del usuario con nombre "Maria" a 140 libras.
+db.usuarios2.updateMany({name:"Maria"},{$set:{weight:140}});
+//9.Incrementar el salario de todos los usuarios que son de "Estados Unidos" en un 15%.
+db.usuarios2.updateMany({country:"Estados Unidos"},{$mul:{salary:1.15}});
+//10.Actualizar el correo electrónico del usuario con nombre "Pedro" a "nuevo_correo@riwi.co".
+db.usuarios2.updateMany({name:"Pedro"},{$set:{email:"nuevo_correo@riwi.co"}});
+//11.Cambiar la edad de todos los usuarios menores de 30 años a 30 años.
+db.usuarios2.updateMany({age:{$lt:30}},{$set:{age:30}});
+//12.Incrementar el salario de los usuarios que tienen un salario inferior a 3000 dólares en un 20%.
+db.usuarios2.updateMany({salary:{lt:3000}},{$mul:{salary:1.2}});
+//13.Actualizar la ciudad de residencia de todos los usuarios que actualmente viven en "Bogotá" a "Medellín".
+db.usuarios2.updateMany({city:"Bogotá"},{$set:{city:"Medellin"}});
+//14.Cambiar el país de residencia de los usuarios con un salario superior a 5000 dólares a "Australia".
+db.usuarios2.updateMany({salary:{$gt:5000}},{$set:{country:"Australia"}})
+//15.Reducir la edad de todos los usuarios que tienen más de 50 años en 5 años.
+db.usuarios2.updateMany({age:{$gt:50}},{$inc:{age:-5}});
+//16.Actualizar el peso de los usuarios que pesan más de 200 libras a 180 libras.
+db.usuarios2.updateMany({weight:{$gt:200}},{$set:{weight:180}})
+//17.Incrementar el salario de los usuarios que viven en "London" en un 25%.
+db.usuarios2.updateMany({city:"London"},{$mul:{salary:1.25}});
+//18.Cambiar el apellido de los usuarios con un salario superior a 4000 dólares a "Smith".
+db.usuarios2.updateMany({salary:{$gt:4000}},{$set:{lastname:"Smith"}})
+//19.Actualizar el correo electrónico de todos los usuarios cuyo nombre empiece por "A" a "nuevo@riwi.es".
+db.usuarios2.updateMany({name: {$regex:/^a/i}},{$set:{email:"nuevo@riwi.es"}});
+//20.Cambiar la ciudad de residencia de los usuarios con una altura inferior a 160 centímetros a "París".
+db.usuarios2.updateMany({height:{$lt:160}},{$set:{city:"París"}})
